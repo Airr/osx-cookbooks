dcc29cba4410dd1901ff34c1f97b5aaffafbeed4
Joshua Peek <josh@joshpeek.com>
2013-06-18 10:31:45 -0500

    More missing deps

--- openvpn/metadata.rb
+++ openvpn/metadata.rb
@@ -1,10 +1,10 @@
 maintainer       "Joshua Peek"
 maintainer_email "josh@joshpeek.com"
 license          "MIT"
-description      "Installs/configures Handbrake"
+description      "Installs/configures OpenVPN"
 version          "0.1.0"
 
-depends "application"
+depends "homebrew"
 depends "archive"
 
 supports "mac_os_x"
