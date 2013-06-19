ab0a6dc120a2e1979215ac8778b97a5cf1a163f0
Joshua Peek <josh@joshpeek.com>
2013-06-19 00:31:54 -0500

    No peepopen

--- emacs/recipes/cocoa.rb
+++ emacs/recipes/cocoa.rb
@@ -3,5 +3,3 @@ include_recipe "emacs"
 application "Emacs" do
   source "http://emacsformacosx.com/emacs-builds/Emacs-24.1-universal-10.6.8.dmg"
 end
-
-include_recipe "peepopen"
--- peepopen/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Joshua Peek"
-maintainer_email "josh@joshpeek.com"
-license          "MIT"
-description      "Installs PeepOpen"
-version          "0.1.0"
-
-depends "application"
-
-supports "mac_os_x"
