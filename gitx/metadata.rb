96e9d8d5030be669dfdd8353da0e5158e86c3cfa
Joshua Peek <josh@joshpeek.com>
2013-10-05 00:44:43 -0500

    Remove gitx cookbook

--- gitx/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Joshua Peek"
-maintainer_email "josh@joshpeek.com"
-license          "MIT"
-description      "Installs GitX"
-version          "0.1.0"
-
-depends "application"
-
-supports "mac_os_x"
