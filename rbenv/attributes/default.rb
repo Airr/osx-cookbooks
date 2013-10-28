d8449caacae9964c458aea22107fba36f775928d
Joshua Peek <josh@joshpeek.com>
2013-10-28 18:58:42 -0500

    rbenv in var

--- rbenv/attributes/default.rb
+++ rbenv/attributes/default.rb
@@ -1,5 +1,5 @@
 default.rbenv[:user]     = ENV['SUDO_USER'] || ENV['USER']
 # default.rbenv[:root]     = ENV['RBENV_ROOT'] || "#{ENV['HOME']}/.rbenv"
-default.rbenv[:root]     = "/usr/local/lib/rbenv"
+default.rbenv[:root]     = "/usr/local/var/rbenv"
 default.rbenv[:versions] = []
 default.rbenv[:global]   = "2.0.0-p247"
