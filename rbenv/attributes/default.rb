bdfffdca725a381b00b65baee75dc4537bf96589
Joshua Peek <josh@joshpeek.com>
2014-03-31 10:33:15 -0500

    Ruby 2.1.1

--- rbenv/attributes/default.rb
+++ rbenv/attributes/default.rb
@@ -2,4 +2,4 @@ default.rbenv[:user]     = ENV['SUDO_USER'] || ENV['USER']
 # default.rbenv[:root]     = ENV['RBENV_ROOT'] || "#{ENV['HOME']}/.rbenv"
 default.rbenv[:root]     = "/usr/local/var/rbenv"
 default.rbenv[:versions] = []
-default.rbenv[:global]   = "2.1.0"
+default.rbenv[:global]   = "2.1.1"
